f16d0e947f1b3617fa65f466312111a38b17576f
Karan25sing <singhkaranf15@gmail.com>
2026-01-29 21:18:54 +0530

    Add app.py and index.html files

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello Git") 
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "red"
+
+@app.route('/grey')
+def indexxx():
+    return "redfghkl;"
+
+if __name__ == "__main__":
+    app.run()
+
